dd622e9e0e6e1a2e57865aa61623a9a844a651d3
kimaruemmanuel25 <kimaruemmanuel25@gmail.com>
2022-09-14 14:07:36 -0700

    BLUES

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * Description: prints the first 98 Fibonacci numbers
+ * Return: Always 0(Success)
+ */
+
+/**
+ * fib - prints the first 98 Fibonacci numbers
+ * @n: n is an integer
+ * Description: prints the first 98 Fibonacci numbers 
+ * Return: integer
+ */
+int fib(int n)
+{
+	if (n <= 1)
+		return (n);
+	return (fib(n - 1)) + (fib(n - 2));
+}
+int main(void)
+{
+	int i = 98;
+
+	printf("%d", fib(i));
+	getchar();
+	return (0);
+}
